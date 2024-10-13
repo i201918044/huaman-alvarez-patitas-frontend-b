31eb532bbc4575c08d080f3bcf43e16f1619725b
Janet Huaman Alvarez <i201918044@cibertec.edu.pe>
2024-10-13 17:46:25 -0500

    Carga de evaluacion final

---
+++ src/main/java/pe/edu/cibertec/huaman_alvarez_patitas_frontend_b/HuamanAlvarezPatitasFrontendBApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.huaman_alvarez_patitas_frontend_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuamanAlvarezPatitasFrontendBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuamanAlvarezPatitasFrontendBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/huaman_alvarez_patitas_frontend_b/HuamanAlvarezPatitasFrontendBApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.huaman_alvarez_patitas_frontend_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuamanAlvarezPatitasFrontendBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
